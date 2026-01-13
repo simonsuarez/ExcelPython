79a0a26b077ddda680224d80a8981b69cf226691
Simon Suarez <simon.suarez.ovalle@gmail.com>
2026-01-12 22:39:16 -0300

    feat: carga de usuarios desde archivo Excel

---
+++ app/data/leerExcel.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from sqlalchemy.orm import Session
+from models.user import Usuario
+from db.conexionBD import SessionLocal
+
+def cargar_excel(path: str):
+    db: Session = SessionLocal()
+    try:
+        df = pd.read_excel(path)
+
+        for _, row in df.iterrows():
+            usuario = Usuario(
+                name=row["name"],
+                lastName=row.get("lastName"),
+                age=row["age"],
+                email=row["email"]
+            )
+            db.add(usuario)
+
+        db.commit()
+        print(f"Insertados {len(df)} registros")
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    cargar_excel("/app/data/usuarios.xlsx")
