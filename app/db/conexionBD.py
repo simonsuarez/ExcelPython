dcd632c7ef053c396c0b6434911e93fafeb4a37d
Simon Suarez <simon.suarez.ovalle@gmail.com>
2026-01-20 12:29:25 -0300

    fix: si no existe la URL de conexion tomara una por defecto

--- app/db/conexionBD.py
+++ app/db/conexionBD.py
@@ -10,7 +10,9 @@ POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "localhost")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
 
 # Crear la URL de conexión
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL", 
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
 # Crear engine y sesión
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
