f8f7ff688005618fd94a91307f7ba59fc3fec4f3
Simon Suarez <simon.suarez.ovalle@gmail.com>
2026-01-20 12:36:54 -0300

    fix: no considere el import en la validacion de Ruff

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,5 +1,6 @@
 from db.base import Base
 from db.conexionBD import engine
-import models.user_model  # Asegura que los modelos se importen para crear las tablas
+# Asegura que los modelos se importen para crear las tablas
+import models.user_model  # noqa: F401
 
 Base.metadata.create_all(bind=engine)
